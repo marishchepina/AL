446021943be5172df8172d1d40434226f560820a
Mariya Shchepina <marishchepina@gmail.com>
2020-07-14 22:02:08 +0300

    audio works but add  audio too many times
    audio works but add  audio too many times

--- js/task1.js
+++ js/task1.js
@@ -1,37 +1,41 @@
 const STEP1 = document.getElementById("task1");
-
 let container = document.getElementById("container");
 let wordUl = document.createElement('ul');
 container.appendChild(wordUl);
-var index = 0;
+let index = 0;
 
 let renderWord = function (word) {
     for (let i = 0; i < word.length; i++) {
         let wordLi = document.createElement('li');
         let wordLi__img = document.createElement('img');
-        var audio = new Audio();
-        audio.src = `audio\\${i+1}.mp3`;
-        audio.play();
         wordUl.appendChild(wordLi);
-        wordLi.appendChild(audio);
-        //audio.setAttribute("muted", "muted");
         wordLi.className = 'js-word';
         wordLi.innerText = word[i].word + ' ' + word[i].translate;
         wordLi.appendChild(wordLi__img);
-        wordLi.appendChild(audio);
+        //wordLi.appendChild(audio);
         wordLi.id = word[i].id;
         wordLi__img.src = word[i].img;
 
-
     }
+}
+let i=0;
 
     function showWord(listOfWords) {
+
+        let list = document.getElementsByClassName("js-word");
+        let audio = new Audio();
+        //let audio = document.createElement('audio');
         li = document.getElementById(wordList[index].id);
         li.classList.add("top");
-        let list = document.getElementsByClassName("js-word");
+        li.appendChild(audio);
+        audio.src = `audio\\${i + 1}.mp3`;
+        audio.play();
+        i++;
+        if (i >= listOfWords.length) {
+            i = 0;
+        }
         for (let item of list) {
             if (item.id == wordList[index].id) {
-
             }
             else {
                 item.classList.remove("top");
@@ -45,18 +49,15 @@ let renderWord = function (word) {
 
     let animate = setInterval(function () {
         showWord(wordList)
-    }, 1000);
+    }, 3000);
 
     function stop() {
         clearInterval(animate);
     }
 
 
-}
 
-
-
-STEP1.onclick = function(){
+STEP1.onclick = function () {
     renderWord(wordList);
     showWord(wordList);
-  }
+}
