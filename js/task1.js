8d37022d3f5facf81f4beadd0d687cd8cf6e7137
Mariya Shchepina <marishchepina@gmail.com>
2020-07-15 11:45:32 +0300

    audio works fine!!!
    audio works fine!!!

--- js/task1.js
+++ js/task1.js
@@ -8,34 +8,37 @@ let renderWord = function (word) {
     for (let i = 0; i < word.length; i++) {
         let wordLi = document.createElement('li');
         let wordLi__img = document.createElement('img');
-        wordUl.appendChild(wordLi);
+        let audio = document.createElement('audio');
+
+        wordLi.id = word[i].id;
         wordLi.className = 'js-word';
         wordLi.innerText = word[i].word + ' ' + word[i].translate;
-        wordLi.appendChild(wordLi__img);
-        //wordLi.appendChild(audio);
-        wordLi.id = word[i].id;
         wordLi__img.src = word[i].img;
+        audio.src = `audio\\${i + 1}.mp3`;
+        audio.className = `audio`;
 
+        wordUl.appendChild(wordLi);
+        wordLi.appendChild(wordLi__img);
+        wordLi.appendChild(audio);
     }
 }
 let i=0;
 
     function showWord(listOfWords) {
-
-        let list = document.getElementsByClassName("js-word");
-        let audio = new Audio();
-        //let audio = document.createElement('audio');
         li = document.getElementById(wordList[index].id);
         li.classList.add("top");
-        li.appendChild(audio);
-        audio.src = `audio\\${i + 1}.mp3`;
-        audio.play();
+        let list = document.getElementsByClassName("js-word");
+        let audiolist = document.getElementsByClassName("audio");
         i++;
         if (i >= listOfWords.length) {
             i = 0;
         }
+
+
         for (let item of list) {
             if (item.id == wordList[index].id) {
+                let audio = document.getElementById(item.id).getElementsByTagName('audio')[0];
+                audio.play();
             }
             else {
                 item.classList.remove("top");
