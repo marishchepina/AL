7e6f4a4bf3e0f806aa8467389d9690078cf1cf29
Mariya Shchepina <marishchepina@gmail.com>
2020-07-17 11:56:04 +0300

    task2 render
    task2 render

--- js/task2.js
+++ js/task2.js
@@ -1,72 +1,51 @@
+const STEP2 = document.getElementById("task2");
 let container = document.getElementById("container");
-let wordUl = document.createElement('ul');
-container.appendChild(wordUl);
+
 
 let renderWord = function (word) {
-    for (let i = 0; i < word.length; i++) {
-        let wordLi = document.createElement('li');
-        let wordLi__img = document.createElement('img');
-        wordUl.appendChild(wordLi);
-        wordLi.className = 'js-word';
-        wordLi.innerText = word[i].word + ' ' + word[i].translate;
-        wordLi.appendChild(wordLi__img);
-        wordLi.id = word[i].id;
-        wordLi__img.src = word[i].img;
-        //setTimeout(function(){wordLi.classList.add("top");}, 500);
+    if (container.children.length > 0) {}
+    else {
+        for (let i = 0; i < word.length; i++) {
+            let img = document.createElement('img');
+            let audio = document.createElement('audio');
 
-    }
 
-    //setTimeout(showWord.bind(word, wordLi), 500);
-}
-/* var i;
- let showWord = function (listOfWords) {
- for (i = 0; i < listOfWords.length; i++) {
- setInterval(() => {
- li = document.getElementById(wordList[i].id);
- li.classList.add("top");
- let list = document.getElementsByClassName("js-word");
- for (let item of list) {
- if (item.id == wordList[i].id){
- //item.classList.add("top");
- // console.log(item.id);
- }
- else {item.classList.remove("top");}
- }
+            img.src = word[i].img;
+            audio.src = `audio\\${i + 1}.mp3`;
+            audio.className = `audio`;
 
- }, 3000);
- }*/
-
-var index = 0;
-function showWord(listOfWords) {
-    li = document.getElementById(wordList[index].id);
-    li.classList.add("top");
-    let list = document.getElementsByClassName("js-word");
-    for (let item of list) {
-        if (item.id == wordList[index].id) {
-        }
-        else {
-            item.classList.remove("top");
+            container.appendChild(img);
+            container.appendChild(audio);
         }
     }
-    index++;
-    if (index >= listOfWords.length) {
-        index = 0;
+
+}
+
+let renderLetters = function (word) {
+    for (let i = 0; i > word.length; i++) {
+        let letters = word[i].word;
+        letters = letters.split('');
+
+
+        function shuffle(arr) {
+            var j, temp;
+            for (var i = arr.length - 1; i > 0; i--) {
+                j = Math.floor(Math.random() * (i + 1));
+                temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+            return arr;
+        }
+        console.log(letters);
+        console.log(shuffle(letters));
+
     }
 }
-let animate = setInterval(function(){showWord(wordList)}, 3000);
-function stop() {
-    clearInterval(animate);
+
+
+STEP2.onclick = function () {
+    renderWord(wordList);
+    renderLetters(wordList);
+
 }
-
-
-//li.classList.remove("top");
-//let  elements = document.getElementsByTagName('li');
-//elements.classList.remove("top");
-
-/* let  li = document.getElementsByClassName('js-word');
- console.log(li);
- li.classList.remove("top");*/
-
-
-renderWord(wordList);
-showWord(wordList);
